3b89682626cca12dd9e3f0fdea1e80d190acf925
smukums <raimis15lvus@yahoo.com>
2026-03-10 10:52:20 -0500

    Delete products from cart when clicking delete

--- javascript-amazon-project/data/cart.js
+++ javascript-amazon-project/data/cart.js
@@ -1,4 +1,4 @@
-export const cart = [{
+export let cart = [{
   productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
   quantity: 2,
 }, {
@@ -23,4 +23,16 @@ cart.forEach((cartItem) => {
     quantity: 1
   });
   }
+}
+
+export function removeFromCart(productId) {
+  const newCart = [];
+
+  cart.forEach((cartItem) => {
+    if (cartItem.product !== productId) {
+      newCart.push(cartItem);
+    }
+  });
+
+  cart = newCart;
 }
