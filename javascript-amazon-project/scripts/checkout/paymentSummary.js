8a1987eb12379fc23985a3bda93c87ec0f55feba
smukums <raimis15lvus@yahoo.com>
2026-03-15 20:23:51 -0500

    Make payment summary interactive

--- javascript-amazon-project/scripts/checkout/paymentSummary.js
+++ javascript-amazon-project/scripts/checkout/paymentSummary.js
@@ -1,5 +1,5 @@
 import {cart} from '../../data/cart.js';
-import {getproduct} from '../../data/products.js';
+import {getProduct} from '../../data/products.js';
 import {getDeliveryOption} from '../../data/deliveryOptions.js';
 import {formatCurrency} from '../utils/money.js';
 
