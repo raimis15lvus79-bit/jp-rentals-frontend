efcaddb5a1d6f0e7ada20eecdedf17f87385b531
smukums <raimis15lvus@yahoo.com>
2026-03-22 16:45:02 -0500

    Convert products into classes

--- javascript-amazon-project/data/products.js
+++ javascript-amazon-project/data/products.js
@@ -1,7 +1,8 @@
+import {formatCurrency} from '../scripts/utils/money.js';
 export function getProduct(productId){
   let matchingProduct;
 
-    products.forEach((product) => {
+    product.forEach((product) => {
       if (product.id === productId) {
         matchingProduct = product;
       }
@@ -10,10 +11,35 @@ export function getProduct(productId){
     return matchingProduct;
 }
 
-export const products = [
+class Product {
+  id;
+  image;
+  name;
+  rating;
+  priceCents;
+
+  constructor(productDetails) {
+    this.id = productDetails.id;
+    this.image = productDetails.image;
+    this.name = productDetails.name;
+    this.rating = productDetails.rating;
+    this.priceCents = productDetails.priceCents;
+  }
+
+  getStarsUrl() {
+   return  `images/ratings/rating-${this.rating.stars * 10}.png`;
+  }
+
+  getPrice() {
+    return `${formatCurrency(this.priceCents)}`;
+  }
+}
+
+export const product = [ 
+  
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    image: "images/products/athletic-cotton-socks-6-pairs.jpg",
+    image: "image/products/athletic-cotton-socks-6-pairs.jpg",
     name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
     rating: {
       stars: 4.5,
@@ -26,6 +52,7 @@ export const products = [
       "apparel"
     ]
   },
+
   {
     id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
     image: "images/products/intermediate-composite-basketball.jpg",
@@ -669,4 +696,6 @@ export const products = [
       "mens"
     ]
   }
-];
+].map((productDetails) => {
+  return new Product(productDetails);
+});
--- javascript-amazon-project/scripts/checkout/orderSummary.js
+++ javascript-amazon-project/scripts/checkout/orderSummary.js
@@ -1,5 +1,5 @@
 import {cart, removeFromCart, updateDeliveryOption, updateQuantity} from '../../data/cart.js';
-import {products, getProduct} from '../../data/products.js';
+import {product, getProduct} from '../../data/products.js';
 import {formatCurrency} from '../utils/money.js';
 import {hello} from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';
 import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js';
@@ -45,7 +45,7 @@ export function renderOrderSummary() {
         ${matchingProduct.name}
       </div>
       <div class="product-price">
-        $${formatCurrency(matchingProduct.priceCents)}
+        ${matchingProduct.getPrice()}
       </div>
       <div class="product-quantity">
         <span>
