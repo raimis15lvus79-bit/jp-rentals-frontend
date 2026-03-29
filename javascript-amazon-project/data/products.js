d9add1b4eb3b7e7a1eebfa88bad5b44d6d5837ef
smukums <raimis15lvus@yahoo.com>
2026-03-28 19:27:04 -0500

    Learn error handling

--- javascript-amazon-project/data/products.js
+++ javascript-amazon-project/data/products.js
@@ -105,10 +105,13 @@ export function loadProductsFetch() {
   });
 
   console.log('load products');
-  });
+  }).catch((error) => {
+    console.log('unexpected error. Please try again later.');
+  }); 
 
   return promise;
 }
+
 
 /*
 loadProductsFetch().then(() => {
@@ -133,9 +136,14 @@ export function loadProducts(fun) {
   fun();
 });
 
+xhr.addEventListener('error', (error) => {
+  console.log('unexpected error. Please try again later.');
+});
+
   xhr.open('GET', 'https://supersimplebackend.dev/products');
   xhr.send();
-}
+} 
+
 
 
 /*
--- javascript-amazon-project/scripts/checkout.js
+++ javascript-amazon-project/scripts/checkout.js
@@ -7,14 +7,23 @@ import {loadCart} from '../data/cart.js';
 
 
 async function loadPage() {
-  await loadProductsFetch();
+  try {
+   // throw 'error';
 
-  const value = await new Promise((resolve) => {
+    await loadProductsFetch();
+
+  const value = await new Promise((resolve, reject) => {
+    // throw 'error2';
     loadCart(() => {
+      // reject('error3');
       resolve('value3');
     });
   });
 
+  } catch (error) {
+    console.log('Unexpected error.  Please try again later.');
+  }
+ 
   renderOrderSummary();
   renderPaymentSummary();
 }
