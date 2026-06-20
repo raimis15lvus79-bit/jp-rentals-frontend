0e95c335057bffc3d74367c2cb7fac8b5dfa4d7e
smukums <raimis15lvus@yahoo.com>
2026-06-20 18:53:02 -0500

    Convert home and checkout to async await

--- react-course/ecommerce-project/src/pages/checkout/CheckoutPage.jsx
+++ react-course/ecommerce-project/src/pages/checkout/CheckoutPage.jsx
@@ -12,17 +12,20 @@ export function CheckoutPage({ cart }) {
   const [paymentSummary, setPaymentSummary] = useState(null);
 
   useEffect(() => {
-    document.title = 'Checkout';
+    const fetchCheckoutData = async () => {
+      let response = await axios.get(
+        '/api/delivery-options?expand=estimatedDeliveryTime'
+      );
+      setDeliveryOptions(response.data);
+      
 
-    axios.get('/api/delivery-options?expand=estimatedDeliveryTime')
-      .then((response) => {
-        setDeliveryOptions(response.data);
-      });
+      response = await axios.get('/api/payment-summary');
+      
+      setPaymentSummary(response.data);
+      
+    };
 
-    axios.get('/api/payment-summary')
-      .then((response) => {
-        setPaymentSummary(response.data);
-      });
+    fetchCheckoutData();
   }, []);
 
   return (
--- react-course/ecommerce-project/src/pages/home/HomePage.jsx
+++ react-course/ecommerce-project/src/pages/home/HomePage.jsx
@@ -9,12 +9,11 @@ const [products, setProducts] = useState([]);
 
   
 useEffect(() => {
-  axios.get('/api/products')
-    .then((response) => {
-    setProducts(response.data);
-  });
-
-  
+  const getHomeData = async () => {
+    const response = await axios.get('/api/products');
+  setProducts(response.data);
+  };
+  getHomeData();
 }, []);
   
 
