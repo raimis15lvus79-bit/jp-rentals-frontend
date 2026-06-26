394fbcbf4de88583f06fc2c2e4ff2e05bfd3f3e8
smukums <raimis15lvus@yahoo.com>
2026-06-25 21:01:14 -0500

    Create update delivery option feature

--- react-course/ecommerce-project/src/pages/checkout/CheckoutPage.jsx
+++ react-course/ecommerce-project/src/pages/checkout/CheckoutPage.jsx
@@ -1,13 +1,11 @@
 import axios from 'axios';
-
 import { useEffect, useState } from 'react';
-
 import { OrderSummary } from './OrderSummary';
 import { PaymentSummary } from './PaymentSummary';
 import './checkout-header.css';
 import './CheckoutPage.css';
 
-export function CheckoutPage({ cart }) {
+export function CheckoutPage({ cart, loadCart }) {
   const [deliveryOptions, setDeliveryOptions] = useState([]);
   const [paymentSummary, setPaymentSummary] = useState(null);
 
@@ -18,15 +16,12 @@ export function CheckoutPage({ cart }) {
       );
       setDeliveryOptions(response.data);
       
-
       response = await axios.get('/api/payment-summary');
-      
       setPaymentSummary(response.data);
-      
     };
 
     fetchCheckoutData();
-  }, []);
+  }, [cart]);
 
   return (
     <>
@@ -57,7 +52,7 @@ export function CheckoutPage({ cart }) {
         <div className="page-title">Review your order</div>
 
         <div className="checkout-grid">
-          <OrderSummary cart={cart} deliveryOptions={deliveryOptions} />
+          <OrderSummary cart={cart} deliveryOptions={deliveryOptions} loadCart={loadCart} />
             <PaymentSummary paymentSummary={paymentSummary} />
         </div>
       </div>
--- react-course/ecommerce-project/src/pages/checkout/OrderSummary.jsx
+++ react-course/ecommerce-project/src/pages/checkout/OrderSummary.jsx
@@ -3,7 +3,7 @@ import { formatMoney } from '../../utils/money';
 import { DeliveryOptions } from './DeliveryOptions';
 
 
-export function OrderSummary({cart, deliveryOptions}) {
+export function OrderSummary({cart, deliveryOptions, loadCart}) {
     return (
       <div className="order-summary">
                   {deliveryOptions.length > 0 && cart.map((cartItem) => {
@@ -49,7 +49,7 @@ export function OrderSummary({cart, deliveryOptions}) {
                             </div>
                           </div>
       
-                          <DeliveryOptions cartItem={cartItem} deliveryOptions={deliveryOptions} />
+                          <DeliveryOptions cartItem={cartItem} deliveryOptions={deliveryOptions} loadCart={loadCart} />
                         </div>
                       </div>
                     );
