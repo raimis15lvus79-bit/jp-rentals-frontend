70b2b3b56613a5e21dbcfba510b48b6977dcf37e
smukums <raimis15lvus@yahoo.com>
2026-07-13 13:08:19 -0500

    creating rental folder

---
+++ rental-business/ecommerce-project/src/pages/checkout/CheckoutPage.jsx
@@ -0,0 +1,62 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { OrderSummary } from './OrderSummary';
+import { PaymentSummary } from './PaymentSummary';
+import './checkout-header.css';
+import './CheckoutPage.css';
+
+export function CheckoutPage({ cart, loadCart }) {
+  const [deliveryOptions, setDeliveryOptions] = useState([]);
+  const [paymentSummary, setPaymentSummary] = useState(null);
+
+  useEffect(() => {
+    const fetchCheckoutData = async () => {
+      let response = await axios.get(
+        '/api/delivery-options?expand=estimatedDeliveryTime'
+      );
+      setDeliveryOptions(response.data);
+      
+      response = await axios.get('/api/payment-summary');
+      setPaymentSummary(response.data);
+    };
+
+    fetchCheckoutData();
+  }, [cart]);
+
+  return (
+    <>
+      <div className="checkout-header">
+        <div className="header-content">
+          <div className="checkout-header-left-section">
+            <a href="/">
+              <img className="logo" src="images/logo.png" alt="Logo" />
+              <img className="mobile-logo" src="images/mobile-logo.png" alt="Mobile logo" />
+            </a>
+          </div>
+
+          <div className="checkout-header-middle-section">
+            Checkout (
+            <a className="return-to-home-link" href="/">
+              {cart.length} items
+            </a>
+            )
+          </div>
+
+          <div className="checkout-header-right-section">
+            <img src="images/icons/checkout-lock-icon.png" alt="Checkout lock" />
+          </div>
+        </div>
+      </div>
+
+      <div className="checkout-page">
+        <div className="page-title">Review your order</div>
+
+        <div className="checkout-grid">
+          <OrderSummary cart={cart} deliveryOptions={deliveryOptions} loadCart={loadCart} />
+          
+          <PaymentSummary paymentSummary={paymentSummary} loadCart={loadCart} />
+        </div>
+      </div>
+    </>
+  );
+}
